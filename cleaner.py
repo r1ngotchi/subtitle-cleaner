3ff7a024766acb319faac0f4715178fdf9490a1f
kanarin4 <rinkanai100@gmail.com>
2026-06-04 22:26:35 +0900

    build subtitle cleaner v0.1

---
+++ cleaner.py
@@ -0,0 +1,26 @@
+import re
+
+print("=== Subtitle Cleaner v0.1 ===")
+
+text = input("Paste subtitle text: ")
+
+# remove repeated words
+text = re.sub(r'\b(\w+)( \1\b)+', r'\1', text)
+
+# remove filler words
+fillers = ["um", "uh", "like"]
+
+words = text.split()
+cleaned_words = []
+
+for word in words:
+    if word.lower() not in fillers:
+        cleaned_words.append(word)
+
+text = " ".join(cleaned_words)
+
+# capitalize first letter
+text = text.capitalize()
+
+print("\n=== Cleaned Output ===\n")
+print(text)
